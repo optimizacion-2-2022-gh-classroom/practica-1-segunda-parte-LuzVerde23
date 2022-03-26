89ad5b700d69866ceb10d597bf798299b8386d4a
EddOselotl <ing.edbaz@gmail.com>
2022-03-26 15:59:05 -0600

    otra prueba para resolver el import de los  packages para test_ff.py #6

--- test/test_ff.py
+++ test/test_ff.py
@@ -4,6 +4,7 @@ from networkx.algorithms.flow import maximum_flow
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import maximum_flow
 from MaxFlowAeiu.MaxFlowAeiu import MaxFlowAeiu
+from src.MaxFlowAeiu.MaxFlowAeiu import MaxFlowAeiu
 
 d = pd.read_csv('BD/d.csv',header=None)
 
