e5a271def42e4ff6ee822889ae0ac399894d38b1
Ita Santiago <50189748+sancas96@users.noreply.github.com>
2022-03-26 21:45:43 -0600

    cambiando collections-ext #5

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,15 @@
 #see: https://setuptools.readthedocs.io/en/latest/userguide/package_discovery.html
 from setuptools import setup, find_packages
 
-setup(name="Maxflow",
+setup(name="MaxFlowAeiu",
       version="0.1",
-      description=u"Optimaztion 2022 team 2 Package practica 1, segunda parte ",
-      url="https://optimizacion-2-2022-gh-classroom.github.io/practica-1-segunda-parte-LuzVerde23/",
+      description=u"Optimization 2022 team 2 Package practica 1, segunda parte ",
+      url="https://optimizacion-2-2022-gh-classroom.github.io/practica-1-segunda-parte-LuzVerde23/index.html",
       author="Team-2",
       author_email="",
       license="MIT",
       packages=find_packages(),
       install_requires = [
-                          "collections"
+                          "collections-extended"
                           ],
       )
