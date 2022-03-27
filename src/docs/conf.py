831194cbb74c3dd54012e91f1240d8471c2dcc16
Ita Santiago <50189748+sancas96@users.noreply.github.com>
2022-03-26 20:57:20 -0600

    Update conf.py #5

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('/home/mirodilla/Documentos/Optimizacion/Tareas/sphinx-for-dummy/src/MaxFlowAeiu/'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../MaxFlowAeiu/'))
 
 
 # -- Project information -----------------------------------------------------
