eea1e34740266c2b13e7b39438fd282887700a89
EddOselotl <ing.edbaz@gmail.com>
2022-03-26 23:53:39 -0600

    Se modifica setup.py para documentacion e implementacion de nuestro paquete MaxFlowAeiu issue #7

--- src/setup.py
+++ src/setup.py
@@ -3,13 +3,17 @@ from setuptools import setup, find_packages
 
 setup(name="MaxFlowAeiu",
       version="0.1",
-      description=u"Optimization 2022 team 2 Package practica 1, segunda parte ",
+      description=u"Execution of the Fork Fulkerson method to find the maximum flow of a network",
+      long_description=open("README.md", "r", encoding="utf-8").read(),
       url="https://optimizacion-2-2022-gh-classroom.github.io/practica-1-segunda-parte-LuzVerde23/index.html",
       author="Team-2",
       author_email="",
+      classifiers = ['License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 3',
+                     'Intended Audience :: Education'
+                     ],
       license="MIT",
+      keywords='max flow',
       packages=find_packages(),
-      install_requires = [
-                          "collections-extended"
-                          ],
+      install_requires = ["collections-extended"]
       )
