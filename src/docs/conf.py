7a6a8cb1400ce8b9f0625e81783b5d61a475642b
sancas96 <ita.sancas@gmail.com>
2022-03-26 19:07:39 -0600

    cambiando ruta .py conf.py #5

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/mirodilla/Documentos/Optimizacion/Tareas/sphinx-for-dummy/src/MaxFlowAeiu/integration/'))
+sys.path.insert(0, os.path.abspath('/home/mirodilla/Documentos/Optimizacion/Tareas/sphinx-for-dummy/src/MaxFlowAeiu/'))
 
 
 # -- Project information -----------------------------------------------------
